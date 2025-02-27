d59066dc1d420ff674c871aeb2295c9ef6f8a3a5
Fabian Philippczyck <fph.risingsunit@gmail.com>
2025-02-27 20:39:46 +0900

    set up blog section
    - set up blog section (needs content)
    - added link and github to projects

--- app/components/Blog/page.tsx
+++ app/components/Blog/page.tsx
@@ -1,15 +1,53 @@
+import Image from "next/image";
+
 export default function Blog() {
+  const blogEntries = [
+    {
+      title: "Mobile Game Development with flame",
+      date: "2025-03-01",
+      summary: "Interested in developing your first mobile game?\nThis is a quick and easy guide of how to get started!",
+      image: "/images/blog1.png",
+      link: "/blog/first-post",
+    },
+    {
+      title: "Another Insightful Article",
+      date: "2025-03-01",
+      summary: "A quick summary of another article that shares interesting insights on a topic.",
+      image: "/images/blog2.jpg",
+      link: "/blog/another-article",
+    },
+    {
+      title: "Latest Tech Trends",
+      date: "2025-03-01",
+      summary: "Exploring the newest trends in technology that are shaping the future.",
+      image: "/images/blog3.jpg",
+      link: "/blog/tech-trends",
+    },
+  ];
+
   return (
-    <section id="blog" className="pt-[120px] bg-white dark:bg-black">
-      <div className="w-[1340px] h-[791px] relative">
-        <div className="w-[420px] h-[791px] left-[920px] top-0 absolute">
-          <div className="w-[420px] h-[341px] left-0 top-[450px] absolute">
-            <div className="w-[420px] h-[60px] left-0 top-[30px] absolute text-center text-[#36c1cf] text-[32px] font-normal font-['Lustria']">Title</div>
-            <div className="w-[420px] h-[60px] left-0 top-[281px] absolute text-center text-[#808080] text-2xl font-normal font-['Lustria']">Read more...</div>
-            <div className="w-[420px] h-[191px] left-0 top-[90px] absolute text-center text-black dark:text-white text-2xl font-normal font-['Lustria']">Summary Summary Summary<br/>Summary Summary Summary<br/>Summary Summary Summary<br/>Summary Summary Summary</div>
-            <div className="w-[420px] h-[30px] left-0 top-0 absolute text-center text-black dark:text-white text-base font-normal font-['Lustria']">Date</div>
-          </div>
-          <div className="w-[420px] h-[450px] left-0 top-0 absolute bg-[#d9d9d9]"></div>
+    <section id="blog" className="pt-8 pb-4 bg-white dark:bg-black">
+      <div className="px-4">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          {blogEntries.map((entry, index) => (
+            <div key={index} className="p-4 border border-gray-300 dark:border-white rounded-lg shadow-lg">
+              <Image 
+                src={entry.image} 
+                alt="Blog image" 
+                width={500} 
+                height={300} 
+                className="rounded-lg"
+              />
+              <div className="text-center text-[#36c1cf] text-[24px] mt-4">{entry.title}</div>
+              <div className="text-center text-black dark:text-white text-sm">{entry.date}</div>
+              <div className="text-center text-black dark:text-white text-lg mt-2">
+                {entry.summary.split("\n").map((line, i) => ( <p key={i}>{line}</p> ))}
+              </div>
+              <div className="text-center text-gray-500 dark:text-gray-300 text-lg mt-2">
+                <a href={entry.link} className="hover:underline">Read more...</a>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </section>
--- app/components/Projects/page.tsx
+++ app/components/Projects/page.tsx
@@ -1,41 +1,57 @@
+"use client";
+
+import { FaGithub } from "react-icons/fa";
+import { useTheme } from "next-themes";
+
 export default function Projects() {
+  const { theme } = useTheme();
+  const isDark = theme === "dark";
+
   const projects = [
     {
       title: "Coming soon...",
+      link: "",
       startDate: "20XX/XX",
       endDate: "20XX/XX",
       summary: "Coming soon...",
       technologies: "Coming soon...",
       achievements: "Coming soon...",
+      github: ""
     },
     {
       title: "Coming soon...",
+      link: "",
       startDate: "20XX/XX",
       endDate: "20XX/XX",
       summary: "Coming soon...",
       technologies: "Coming soon...",
       achievements: "Coming soon...",
+      github: ""
     },
     {
       title: "Coming soon...",
+      link: "",
       startDate: "2025/3",
       endDate: "ongoing",
       summary: "Developing and publishing a popular mobile game.",
       technologies: "flutter with flame engine",
       achievements: "Coming soon...",
+      github: ""
     },
     {
       title: "RisingSunIT website",
+      link: "https://www.risingSunIt.com",
       startDate: "2025/1",
       endDate: "2025/2",
       summary: "Setting up and hosting this website. The start of our professional journey!",
       technologies: "Next.js, React, Typescript, animations with CSS",
       achievements: "✅ Establishing a strong online presence within a minimal timeframe",
+      github: "https://github.com/RisingSunIT/risingSunITWebsite"
     },
   ];
 
   return (
-    <section id="projects" className="dark:bg-black w-[1440px] relative overflow-hidden">
+    <section id="projects" className="dark:bg-black relative overflow-hidden">
       {projects.map((project, index) => {
         const isFirst = index === 0;
         const isLast = index === projects.length - 1;
@@ -47,19 +63,25 @@ export default function Projects() {
             id="projectRow"
             className={`m-16 flex ${isFirst ? "mt-8" : ""} ${isLast ? "mb-8" : ""}`}
           >
-          <div
-            id="projectPill"
+          <div id="projectPill"
             className="bg-[#36c1cf] relative overflow-hidden rounded-[30px] w-[35%] p-4 flex items-center text-center text-white dark:text-black transition-shadow duration-300"
-            style={{ boxShadow: shadowSize }}
-          >
-          {/* Glare Effect */}
-          <div className="absolute top-0 left-[-100%] w-[120%] h-full bg-gradient-to-r from-transparent via-white/60 to-transparent dark:via-black/60 opacity-0 skew-x-[-20deg] 
-                        transition-opacity duration-300 hover:opacity-100 hover:animate-glare infinite"></div>
-          <div className="text-2xl w-2/3">{project.title}</div>
-          <div className="text-xl w-1/3">
-            <p>{project.startDate} ~</p>
-            <p className="pr-4">{project.endDate}</p>
-          </div>
+            style={{ boxShadow: shadowSize }} >
+            {/* Glare Effect */}
+            <div className="absolute top-0 left-[-100%] w-[120%] h-full bg-gradient-to-r from-transparent via-white/60 to-transparent dark:via-black/60 opacity-0 skew-x-[-20deg] 
+                          transition-opacity duration-300 hover:opacity-100 hover:animate-glare infinite"></div>
+            <div className="text-2xl w-1/2"><a href={project.link}>{project.title}</a></div>
+            <div className="w-1/6 flex justify-center items-center">
+              <a href={project.github} target="_blank" rel="noopener noreferrer">
+                <FaGithub 
+                  className="text-3xl cursor-pointer hover:text-gray-500 transition-colors" 
+                  style={{ color: isDark ? "black" : "white" }} 
+                />
+              </a>
+            </div>
+            <div className="text-xl w-1/3">
+              <p>{project.startDate} ~</p>
+              <p className="pr-4">{project.endDate}</p>
+            </div>
           </div>
             <div id="spacer" className="w-[5%]"></div>
             <div className="w-[60%] dark:text-white text-2xl">
